9ff43a6da9712a27e6eaa316226d0ef1a72f6a54
chiuliu <chiuliuhn@gmail.com>
2024-05-06 06:19:39 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+// câu lệnh có sẵn
+
+// Array.indexof()
+// let userInput = +prompt("nhập số")
+// const numbers = [1,2,3,4,5]
+// let result = numbers.indexOf(userInput);
+
+// Array.find()
+// let result =numbers.find((element, index) => element === userInput)
+// console.log(result);
+
+// Array.findIndex()
+let result =numbers.findIndex((element, index) => element === userInput)
+console.log(result);
+
+// Math.max(...array)
